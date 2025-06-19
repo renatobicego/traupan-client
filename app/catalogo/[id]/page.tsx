235c527ea2f1c207c05e886897c14034e99cf915
renatobicego <renatobicego@gmail.com>
2025-06-18 22:55:27 -0300

    added catalogs

--- app/catalogo/[id]/page.tsx
+++ app/catalogo/[id]/page.tsx
@@ -3,12 +3,12 @@ import { getCatalogById } from "@/utils/utils";
 import { Link } from "@heroui/link";
 import Image from "next/image";
 
-export default function CatalogDetailPage({
+export default async function CatalogDetailPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const id = await params.then((p) => p.id);
   const catalogItem = getCatalogById(id);
   if (!catalogItem) {
     return <p>Catalog item not found</p>;
@@ -23,13 +23,13 @@ export default function CatalogDetailPage({
         className="absolute top-[60vh] md:top-[40vh]"
         unoptimized
       />
-      <section className="flex flex-col items-start justify-center gap-8 ">
+      <section className="flex flex-col items-start justify-center gap-16 ">
         <h1>
           {catalogItem.shortTitle
             ? ` ${catalogItem.shortTitle}`
             : "Detalle del Catálogo"}
         </h1>
-        <menu className="flex flex-col items-start justify-start gap-2">
+        <menu className="flex flex-col items-start justify-start gap-4">
           {catalogItem.catalogUrls.map((url, index) => (
             <Button href={url.url} key={index} target="_blank" as={Link}>
               Ver Catálogo {url.title || `${index + 1}`}
