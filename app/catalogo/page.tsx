8e5008ef17cdd739cd33c1f8167bc09ea9a50189
renatobicego <renatobicego@gmail.com>
2025-06-24 20:08:37 -0300

    :

--- app/catalogo/page.tsx
+++ app/catalogo/page.tsx
@@ -10,10 +10,18 @@ export default function CatalogoPage() {
         alt="Fondo de Traupan con logo"
         width={1339}
         height={1339}
-        className="absolute top-[60vh] md:top-[40vh]"
+        className="absolute max-md:object-cover max-md:h-screen top-[40vh]"
         unoptimized
       />
-      <section className="flex flex-col items-center justify-center gap-4 ">
+      <Image
+        src="/fondo-traupan.png"
+        alt="Fondo de Traupan con logo"
+        width={1339}
+        height={1339}
+        className="absolute object-cover h-screen top-[150vh] md:hidden"
+        unoptimized
+      />
+      <section className="flex flex-col items-center justify-center gap-4 mt-8 mb-16">
         <h1 className="self-start">Nuestro Catálogo</h1>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {catalogItems.catalog.map((item) => (
--- components/catalog-card.tsx
+++ components/catalog-card.tsx
@@ -17,20 +17,20 @@ interface CatalogCardProps {
 const CatalogCard = ({ item }: CatalogCardProps) => {
   return (
     <Card className="bg-white" shadow="sm">
-      <CardBody className="overflow-visible p-0">
+      <CardBody className="overflow-visible p-0 flex-initial">
         <Image
           alt={item.title}
-          className="w-full object-cover h-[30vh]"
+          className="w-full object-cover lg:h-[30vh]"
           radius="lg"
           shadow="sm"
           src={item.imgCover}
           width="100%"
         />
       </CardBody>
-      <CardFooter className="text-secondary flex flex-col items-center justify-center gap-2 p-4">
+      <CardFooter className="text-secondary flex flex-col items-center justify-start gap-2 p-4 flex-1">
         <Image alt={item.title} src={item.imgLogo} width={120} />
         <b className="whitespace-pre-line text-center">{item.title}</b>
-        <Button as={Link} href={item.url} target="_blank">
+        <Button as={Link} href={item.url} target="_blank" className="mt-auto">
           Ver Catálogo
         </Button>
       </CardFooter>
